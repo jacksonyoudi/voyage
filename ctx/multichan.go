22a7548b41568263dc957c4cb9e10665b04db744
Changyou.Liang <changyou.liang@shopee.com>
2020-06-01 12:59:49 +0800

    multi chan

---
+++ ctx/multichan.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"time"
+)
+
+func main() {
+	ctx, cancel := context.WithCancel(context.Background())
+	go watch(ctx, "one")
+	go watch(ctx, "two")
+	go watch(ctx, "three")
+
+	time.Sleep(10 * time.Second)
+	fmt.Println("可以了，通知监控停止")
+	cancel()
+	//为了检测监控过是否停止，如果没有监控输出，就表示停止了
+	time.Sleep(5 * time.Second)
+
+}
+
+func watch(ctx context.Context, name string) {
+	for {
+		select {
+		case <-ctx.Done():
+			fmt.Println(name, "任务取消")
+			return
+		default:
+			fmt.Println(name, "working")
+			time.Sleep(time.Second * 1)
+
+		}
+	}
+}
