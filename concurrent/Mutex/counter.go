f7e9cc08a4812badac13761261c1faab9bf6c3ff
jacksonyoudi <liangchangyoujackson@gmail.com>
2020-10-23 23:25:54 +0800

    mutex

---
+++ concurrent/Mutex/counter.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+func main() {
+	//互斥锁保护计数器
+	var mu sync.Mutex
+	var count = 0
+
+
+	var wg sync.WaitGroup
+
+	wg.Add(10)
+
+	for i := 0; i < 10; i++ {
+		go func() {
+			defer wg.Done()
+			for j := 0; j < 1000000; j++ {
+				mu.Lock()
+				count++
+				mu.Unlock()
+			}
+		}()
+	}
+
+	wg.Wait()
+	fmt.Println(count)
+
+}
