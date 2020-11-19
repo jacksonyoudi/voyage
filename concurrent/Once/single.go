d86914df95e3f05687b69dbd3f2ca35b77994c3e
jacksonyoudi <liangchangyoujackson@gmail.com>
2020-11-19 23:17:50 +0800

    once

---
+++ concurrent/Once/single.go
@@ -0,0 +1,21 @@
+package Once
+
+import "time"
+
+var startTime = time.Now()
+
+var startTime1 time.Time
+
+func init() {
+	startTime1 = time.Now()
+}
+
+var startTime2 time.Time
+
+func initApp() {
+	startTime2 = time.Now()
+}
+
+func main() {
+	initApp()
+}
